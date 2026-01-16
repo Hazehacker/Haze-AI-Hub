517bdf2c1cdc43ff25b7f352edb5f65cc976540f
HazeHacker <3542495583@qq.com>
2026-01-16 21:27:28 +0800

    fix: 补全mp配置，升级版本
    - 升级MyBatis Plus版本到3.5.7，解决MyBatis Plus 与 Spring Boot 3.3.7 的兼容性问题

--- backend/ai-server/src/main/java/top/hazenix/hazeaihub/AiServerApplication.java
+++ backend/ai-server/src/main/java/top/hazenix/hazeaihub/AiServerApplication.java
@@ -1,11 +1,9 @@
 package top.hazenix.hazeaihub;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("top.hazenix.hazeaihub.mapper")
 public class AiServerApplication {
 
     public static void main(String[] args) {
